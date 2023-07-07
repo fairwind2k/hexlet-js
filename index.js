1e927f3845262638e4eed7d2ff3a8bc3b02584d8
fairwind2k <fairwind@md2k.net>
2023-07-08 00:51:04 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 // Пересечение массивов
 _.intersection([2, 1], [2, 3]); // => [2]
 
 // capitalize делает первую букву заглавной
-_.capitalize("hello"); // Hello
+_.capitalize('hello'); // Hello
 
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+const a = (arr) => {
+  const b = _.last(arr);
+  return b;
+};
+
+console.log(a(['one', 'two']));
+console.log('Hello, Hexlet!');
